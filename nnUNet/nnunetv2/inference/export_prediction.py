a4879d1c6d4d7a43b0e1322ee2d7d1dd4826962c
Andreas With Aspe <awias@dtu.dk>
2025-12-17 11:50:57 +0100

    nnUNet code upload

---
+++ nnUNet/nnunetv2/inference/export_prediction.py
@@ -0,0 +1,280 @@
+from typing import Union, List
+
+import numpy as np
+import torch
+from acvl_utils.cropping_and_padding.bounding_boxes import insert_crop_into_image
+from batchgenerators.utilities.file_and_folder_operations import load_json, save_pickle
+
+from nnunetv2.configuration import default_num_processes
+from nnunetv2.training.dataloading.nnunet_dataset import nnUNetDatasetBlosc2
+from nnunetv2.utilities.label_handling.label_handling import LabelManager
+from nnunetv2.utilities.plans_handling.plans_handler import PlansManager, ConfigurationManager
+
+import time
+import os
+
+
+
+def convert_predicted_logits_to_segmentation_with_correct_shape(predicted_logits: Union[torch.Tensor, np.ndarray],
+                                                                plans_manager: PlansManager,
+                                                                configuration_manager: ConfigurationManager,
+                                                                label_manager: LabelManager,
+                                                                properties_dict: dict,
+                                                                return_probabilities: bool = False,
+                                                                num_threads_torch: int = default_num_processes, subject_name = None):
+    
+    old_threads = torch.get_num_threads()
+    torch.set_num_threads(num_threads_torch)
+    
+    
+    # print("Predicted logits before reshaping:")
+    # print(predicted_logits.shape)
+    # print("\n")
+    
+    # for i in range(10):
+    #     print(i)
+    #     time.sleep(1)
+
+    # resample to original shape
+    spacing_transposed = [properties_dict['spacing'][i] for i in plans_manager.transpose_forward]
+    current_spacing = configuration_manager.spacing if \
+        len(configuration_manager.spacing) == \
+        len(properties_dict['shape_after_cropping_and_before_resampling']) else \
+        [spacing_transposed[0], *configuration_manager.spacing]
+    predicted_logits = configuration_manager.resampling_fn_probabilities(predicted_logits,
+                                            properties_dict['shape_after_cropping_and_before_resampling'],
+                                            current_spacing,
+                                            [properties_dict['spacing'][i] for i in plans_manager.transpose_forward])
+    
+    # print("Predicted logits after reshaping:")
+    # print(predicted_logits.shape)
+    # print("\n")
+    
+    # for i in range(10):
+    #     print(i)
+    #     time.sleep(1)
+    
+    # Save logits
+    # outputpath = "/scratch/awias/data/Pancreas/nnUNet_dataset/nnUNet_raw/Dataset001_Pancreas/imagesTs/predicted_logits_ts"
+    # os.makedirs(outputpath, exist_ok=True)
+    # subject_name_cleaned = subject_name.split('_')[0]
+    # # Save predicted logits for debugging or analysis
+    # np.savez_compressed(os.path.join(outputpath, subject_name_cleaned+".npz"), logits=predicted_logits)
+    # print("Logits saved")
+
+    # return value of resampling_fn_probabilities can be ndarray or Tensor but that does not matter because
+    # apply_inference_nonlin will convert to torch
+    if not return_probabilities:
+        # this has a faster computation path becasue we can skip the softmax in regular (not region based) trainig
+        segmentation = label_manager.convert_logits_to_segmentation(predicted_logits)
+    else:
+        predicted_probabilities = label_manager.apply_inference_nonlin(predicted_logits)
+        segmentation = label_manager.convert_probabilities_to_segmentation(predicted_probabilities)
+    del predicted_logits
+
+    # put segmentation in bbox (revert cropping)
+    segmentation_reverted_cropping = np.zeros(properties_dict['shape_before_cropping'],
+                                              dtype=np.uint8 if len(label_manager.foreground_labels) < 255 else np.uint16)
+    segmentation_reverted_cropping = insert_crop_into_image(segmentation_reverted_cropping, segmentation, properties_dict['bbox_used_for_cropping'])
+    del segmentation
+
+    # segmentation may be torch.Tensor but we continue with numpy
+    if isinstance(segmentation_reverted_cropping, torch.Tensor):
+        segmentation_reverted_cropping = segmentation_reverted_cropping.cpu().numpy()
+
+    # revert transpose
+    segmentation_reverted_cropping = segmentation_reverted_cropping.transpose(plans_manager.transpose_backward)
+    if return_probabilities:
+        # revert cropping
+        predicted_probabilities = label_manager.revert_cropping_on_probabilities(predicted_probabilities,
+                                                                                 properties_dict[
+                                                                                     'bbox_used_for_cropping'],
+                                                                                 properties_dict[
+                                                                                     'shape_before_cropping'])
+        predicted_probabilities = predicted_probabilities.cpu().numpy()
+        # revert transpose
+        predicted_probabilities = predicted_probabilities.transpose([0] + [i + 1 for i in
+                                                                           plans_manager.transpose_backward])
+        torch.set_num_threads(old_threads)
+        return segmentation_reverted_cropping, predicted_probabilities
+    else:
+        torch.set_num_threads(old_threads)
+        return segmentation_reverted_cropping
+
+
+def export_prediction_from_logits(predicted_array_or_file: Union[np.ndarray, torch.Tensor], properties_dict: dict,
+                                  configuration_manager: ConfigurationManager,
+                                  plans_manager: PlansManager,
+                                  dataset_json_dict_or_file: Union[dict, str], output_file_truncated: str,
+                                  save_probabilities: bool = False, subject_name=None,
+                                  num_threads_torch: int = default_num_processes):
+    # if isinstance(predicted_array_or_file, str):
+    #     tmp = deepcopy(predicted_array_or_file)
+    #     if predicted_array_or_file.endswith('.npy'):
+    #         predicted_array_or_file = np.load(predicted_array_or_file)
+    #     elif predicted_array_or_file.endswith('.npz'):
+    #         predicted_array_or_file = np.load(predicted_array_or_file)['softmax']
+    #     os.remove(tmp)
+    
+    if isinstance(dataset_json_dict_or_file, str):
+        dataset_json_dict_or_file = load_json(dataset_json_dict_or_file)
+        
+    label_manager = plans_manager.get_label_manager(dataset_json_dict_or_file)
+
+    if os.environ.get('DO_NOT_USE_SOFTMAX') == '1':
+        label_manager.inference_nonlin = lambda x: x
+
+    ret = convert_predicted_logits_to_segmentation_with_correct_shape(
+        predicted_array_or_file, plans_manager, configuration_manager, label_manager, properties_dict,
+        return_probabilities=save_probabilities, num_threads_torch=num_threads_torch, subject_name=subject_name
+    )
+    del predicted_array_or_file
+
+    # save
+    if save_probabilities:
+        segmentation_final, probabilities_final = ret
+        np.savez_compressed(output_file_truncated + '.npz', probabilities=probabilities_final)
+        save_pickle(properties_dict, output_file_truncated + '.pkl')
+        del probabilities_final, ret
+    else:
+        segmentation_final = ret
+        del ret
+
+    rw = plans_manager.image_reader_writer_class()
+    rw.write_seg(segmentation_final, output_file_truncated + dataset_json_dict_or_file['file_ending'],
+                 properties_dict)
+
+
+def resample_and_save(predicted: Union[torch.Tensor, np.ndarray], target_shape: List[int], output_file: str,
+                      plans_manager: PlansManager, configuration_manager: ConfigurationManager, properties_dict: dict,
+                      dataset_json_dict_or_file: Union[dict, str], num_threads_torch: int = default_num_processes,
+                      dataset_class=None) \
+        -> None:
+
+    old_threads = torch.get_num_threads()
+    torch.set_num_threads(num_threads_torch)
+
+    if isinstance(dataset_json_dict_or_file, str):
+        dataset_json_dict_or_file = load_json(dataset_json_dict_or_file)
+
+    spacing_transposed = [properties_dict['spacing'][i] for i in plans_manager.transpose_forward]
+    # resample to original shape
+    current_spacing = configuration_manager.spacing if \
+        len(configuration_manager.spacing) == len(properties_dict['shape_after_cropping_and_before_resampling']) else \
+        [spacing_transposed[0], *configuration_manager.spacing]
+    target_spacing = configuration_manager.spacing if len(configuration_manager.spacing) == \
+        len(properties_dict['shape_after_cropping_and_before_resampling']) else \
+        [spacing_transposed[0], *configuration_manager.spacing]
+    predicted_array_or_file = configuration_manager.resampling_fn_probabilities(predicted,
+                                                                                target_shape,
+                                                                                current_spacing,
+                                                                                target_spacing)
+    
+
+    # create segmentation (argmax, regions, etc)
+    label_manager = plans_manager.get_label_manager(dataset_json_dict_or_file)
+    segmentation = label_manager.convert_logits_to_segmentation(predicted_array_or_file)
+    # segmentation may be torch.Tensor but we continue with numpy
+    if isinstance(segmentation, torch.Tensor):
+        segmentation = segmentation.cpu().numpy()
+
+    if dataset_class is None:
+        nnUNetDatasetBlosc2.save_seg(segmentation.astype(dtype=np.uint8 if len(label_manager.foreground_labels) < 255 else np.uint16), output_file)
+    else:
+        dataset_class.save_seg(segmentation.astype(dtype=np.uint8 if len(label_manager.foreground_labels) < 255 else np.uint16), output_file)
+    torch.set_num_threads(old_threads)
+
+
+
+
+
+
+
+
+
+
+# MODIFIED FUNCTIONS TO GET VARIANCE
+# Andreas made these
+def convert_predicted_logits_to_segmentation_with_correct_shape_variance(predicted_logits: Union[torch.Tensor, np.ndarray],
+                                                                plans_manager: PlansManager,
+                                                                configuration_manager: ConfigurationManager,
+                                                                label_manager: LabelManager,
+                                                                properties_dict: dict,
+                                                                return_probabilities: bool = False,
+                                                                num_threads_torch: int = default_num_processes, subject_name = None):
+    
+    old_threads = torch.get_num_threads()
+    torch.set_num_threads(num_threads_torch)
+    
+    
+    # print("Predicted logits before reshaping:")
+    # print(predicted_logits.shape)
+    # print("\n")
+    
+    # for i in range(10):
+    #     print(i)
+    #     time.sleep(1)
+
+    # resample to original shape
+    spacing_transposed = [properties_dict['spacing'][i] for i in plans_manager.transpose_forward]
+    current_spacing = configuration_manager.spacing if \
+        len(configuration_manager.spacing) == \
+        len(properties_dict['shape_after_cropping_and_before_resampling']) else \
+        [spacing_transposed[0], *configuration_manager.spacing]
+    predicted_logits = configuration_manager.resampling_fn_probabilities(predicted_logits,
+                                            properties_dict['shape_after_cropping_and_before_resampling'],
+                                            current_spacing,
+                                            [properties_dict['spacing'][i] for i in plans_manager.transpose_forward])
+    
+
+    
+    predicted_pixel_variances = label_manager.apply_inference_nonlin(predicted_logits)
+    del predicted_logits
+    
+    # revert cropping
+    predicted_pixel_variances = label_manager.revert_cropping_on_probabilities(predicted_pixel_variances,
+                                                                                properties_dict[
+                                                                                    'bbox_used_for_cropping'],
+                                                                                properties_dict[
+                                                                                    'shape_before_cropping'])
+    predicted_pixel_variances = predicted_pixel_variances.cpu().numpy()
+    # revert transpose
+    predicted_pixel_variances = predicted_pixel_variances.transpose([0] + [i + 1 for i in
+                                                                        plans_manager.transpose_backward])
+    torch.set_num_threads(old_threads)
+    return predicted_pixel_variances
+
+
+
+def export_prediction_from_logits_variance(predicted_array_or_file: Union[np.ndarray, torch.Tensor], properties_dict: dict,
+                                  configuration_manager: ConfigurationManager,
+                                  plans_manager: PlansManager,
+                                  dataset_json_dict_or_file: Union[dict, str], output_file_truncated: str,
+                                  save_probabilities: bool = False, subject_name=None,
+                                  num_threads_torch: int = default_num_processes):
+    # if isinstance(predicted_array_or_file, str):
+    #     tmp = deepcopy(predicted_array_or_file)
+    #     if predicted_array_or_file.endswith('.npy'):
+    #         predicted_array_or_file = np.load(predicted_array_or_file)
+    #     elif predicted_array_or_file.endswith('.npz'):
+    #         predicted_array_or_file = np.load(predicted_array_or_file)['softmax']
+    #     os.remove(tmp)
+    
+    if isinstance(dataset_json_dict_or_file, str):
+        dataset_json_dict_or_file = load_json(dataset_json_dict_or_file)
+        
+    label_manager = plans_manager.get_label_manager(dataset_json_dict_or_file)
+
+    # Always just do this. Will be wrong to do softmax again
+    label_manager.inference_nonlin = lambda x: x
+
+    predicted_pixel_variances = convert_predicted_logits_to_segmentation_with_correct_shape_variance(
+        predicted_array_or_file, plans_manager, configuration_manager, label_manager, properties_dict,
+        return_probabilities=save_probabilities, num_threads_torch=num_threads_torch, subject_name=subject_name
+    )
+    del predicted_array_or_file
+
+    # save
+    np.savez_compressed(output_file_truncated + '.npz', probabilities=predicted_pixel_variances)
+    save_pickle(properties_dict, output_file_truncated + '.pkl')
+    del predicted_pixel_variances
---
+++ nnUNet/nnunetv2/utilities/get_network_from_plans.py
@@ -0,0 +1,99 @@
+import pydoc
+import warnings
+from typing import Union
+import pickle
+from nnunetv2.utilities.find_class_by_name import recursive_find_python_class
+from batchgenerators.utilities.file_and_folder_operations import join
+import sys
+
+def get_network_from_plans(arch_class_name, arch_kwargs, arch_kwargs_req_import, input_channels, output_channels,
+                           allow_init=True, deep_supervision: Union[bool, None] = None, dataset_name_or_id: str = None):
+    network_class = arch_class_name
+    architecture_kwargs = dict(**arch_kwargs)
+    for ri in arch_kwargs_req_import:
+        if architecture_kwargs[ri] is not None:
+            architecture_kwargs[ri] = pydoc.locate(architecture_kwargs[ri])
+
+    nw_class = pydoc.locate(network_class)
+    # sometimes things move around, this makes it so that we can at least recover some of that
+    if nw_class is None:
+        warnings.warn(f'Network class {network_class} not found. Attempting to locate it within '
+                      f'dynamic_network_architectures.architectures...')
+        import dynamic_network_architectures
+        nw_class = recursive_find_python_class(join(dynamic_network_architectures.__path__[0], "architectures"),
+                                               network_class.split(".")[-1],
+                                               'dynamic_network_architectures.architectures')
+        if nw_class is not None:
+            print(f'FOUND IT: {nw_class}')
+        else:
+            raise ImportError('Network class could not be found, please check/correct your plans file')
+
+    if deep_supervision is not None:
+        architecture_kwargs['deep_supervision'] = deep_supervision
+
+    network = nw_class(
+        input_channels=input_channels,
+        num_classes=output_channels,
+        **architecture_kwargs
+    )
+
+    info_dict = {
+        'type': type(network),
+        'arch_class_name': arch_class_name,
+        'input_channels': input_channels,
+        'num_classes': output_channels,
+        **architecture_kwargs
+    }
+
+    # path = "/scratch/awias/data/nnUNet/info_dict_verse.pkl"
+    # path = "/scratch/awias/data/Pancreas/info_dict.pkl"
+    # path = "/home/awias/data/nnUNet/info_dict_verse.pkl"
+    # path = "/scratch/awias/data/nnUNet/info_dict_TotalSegmentatorPancreas.pkl"
+    path = f"/scratch/awias/data/nnUNet/info_dict_{dataset_name_or_id}.pkl"
+    try:
+        with open(path, 'wb') as handle:
+            pickle.dump(info_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    except:
+        print(f"ERROR: Peter and I hardcoded this. We try to save the info_dict as a pickle. Please update the path to the correct dataset. Current path defined is: {path}")
+        print(f"You can correct it in this file: {__file__}")
+        sys.exit(1)
+    
+    
+
+    if hasattr(network, 'initialize') and allow_init:
+        network.apply(network.initialize)
+
+    return network
+
+
+if __name__ == "__main__":
+    import torch
+
+
+    model = get_network_from_plans(
+        arch_class_name="dynamic_network_architectures.architectures.unet.ResidualEncoderUNet",
+        arch_kwargs={
+            "n_stages": 7,
+            "features_per_stage": [32, 64, 128, 256, 512, 512, 512],
+            "conv_op": "torch.nn.modules.conv.Conv2d",
+            "kernel_sizes": [[3, 3], [3, 3], [3, 3], [3, 3], [3, 3], [3, 3], [3, 3]],
+            "strides": [[1, 1], [2, 2], [2, 2], [2, 2], [2, 2], [2, 2], [2, 2]],
+            "n_blocks_per_stage": [1, 3, 4, 6, 6, 6, 6],
+            "n_conv_per_stage_decoder": [1, 1, 1, 1, 1, 1],
+            "conv_bias": True,
+            "norm_op": "torch.nn.modules.instancenorm.InstanceNorm2d",
+            "norm_op_kwargs": {"eps": 1e-05, "affine": True},
+            "dropout_op": None,
+            "dropout_op_kwargs": None,
+            "nonlin": "torch.nn.LeakyReLU",
+            "nonlin_kwargs": {"inplace": True},
+        },
+        arch_kwargs_req_import=["conv_op", "norm_op", "dropout_op", "nonlin"],
+        input_channels=1,
+        output_channels=4,
+        allow_init=True,
+        deep_supervision=True,
+    )
+    data = torch.rand((8, 1, 256, 256))
+    target = torch.rand(size=(8, 1, 256, 256))
+    outputs = model(data) # this should be a list of torch.Tensor
